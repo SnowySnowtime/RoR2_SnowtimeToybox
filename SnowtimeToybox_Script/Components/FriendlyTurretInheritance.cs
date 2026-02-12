020938368b618cee544504a9a5b423be0affa566
Snowy Snowtime <snowysnowtime1@gmail.com>
2026-02-12 00:15:47 -0800

    .score inheritance component rework

--- SnowtimeToybox_Script/Components/FriendlyTurretInheritance.cs
+++ SnowtimeToybox_Script/Components/FriendlyTurretInheritance.cs
@@ -3,6 +3,7 @@ using R2API;
 using RoR2;
 using RoR2.EntityLogic;
 using SnowtimeToybox;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -29,80 +30,99 @@ namespace SnowtimeToybox.Components
         {
             self = GetComponent<CharacterMaster>();
             turret = self.GetBody().baseNameToken;
+            FriendlyTurretMirrorInventory();
+        }
+        public void Awake()
+        {
+            enabled = NetworkServer.active;
         }
 
         public void FixedUpdate()
         {
-            if (!ownerInventory)
-            {
-                if (self.minionOwnership && self.minionOwnership.ownerMaster)
-                {
-                    ownerInventory = self.minionOwnership.ownerMaster.inventory;
-                    ownerInventory.onInventoryChanged += FriendlyTurretMirrorInventory;
-                    FriendlyTurretMirrorInventory();
-                }
-            }
             if (self.inventory.GetItemCountPermanent(RoR2Content.Items.MinionLeash) > 0) return;
             self.inventory.GiveItemPermanent(RoR2Content.Items.MinionLeash);
         }
-
-        private void FriendlyTurretMirrorInventory()
+        public void FriendlyTurretMirrorInventory()
         {
-            List<ItemInfo> currentState = BuildInventoryState(self.inventory);
-            EquipmentIndex currentEquipIndex = self.inventory.currentEquipmentIndex;
-            List<ItemInfo> ownerState = BuildInventoryState(ownerInventory);
-            EquipmentIndex ownerEquipIndex = self.minionOwnership.ownerMaster.inventory.currentEquipmentIndex;
-            //Log.Debug("Inventory Updated...");
-            self.inventory.CopyItemsFrom(ownerInventory, ItemFilter);
+            if (self?.inventory == null)
+                return;
 
-            foreach (ItemInfo info in currentState)
+            var ownerInventory = self.minionOwnership?.ownerMaster?.inventory;
+            if (ownerInventory == null)
+                return;
+
+            ChannelOwnerInventory(self, ownerInventory);
+
+            EquipmentIndex ownerEquipment = ownerInventory.currentEquipmentIndex;
+            if (!SnowtimeToyboxMod.eliteDefsEquipInherit.Contains(ownerEquipment))
             {
-                if (self.inventory.GetItemCountPermanent(info.index) < info.count)
-                {
-                    if (!(LastOwnerInventoryState.Where(x => x.index == info.index).Count() >= info.count && ownerState.Where(x => x.index == info.index).Count() < info.count))
-                    {
-                        self.inventory.RemoveItemPermanent(info.index, self.inventory.GetItemCountPermanent(info.index));
-                        self.inventory.GiveItemPermanent(info.index, info.count);
-                    }
-                }
+                ownerEquipment = EquipmentIndex.None;
             }
 
-            if (currentEquipIndex != ownerEquipIndex)
+            if (self.inventory.currentEquipmentIndex != ownerEquipment)
             {
-                //Log.Debug("Checking if Equipment is an Elite Equipment");
-                //Log.Debug(SnowtimeToyboxMod.eliteDefsEquipInherit.Contains(ownerEquipIndex));
-                //Log.Debug("Owner Equipment Index: " + ownerEquipIndex);
-                if (!SnowtimeToyboxMod.eliteDefsEquipInherit.Contains(ownerEquipIndex))
-                {
-                    //Log.Debug("Check Failed, checking if we have equipment to begin with.");
-                    if (currentEquipIndex == EquipmentIndex.None) return;
-                    //Log.Debug("We had an equipment prior, removing equipment.");
-                    self.inventory.SetEquipmentIndex(EquipmentIndex.None, true);
-                }
-                else
-                {
-                    //Log.Debug("Check Passed.");
-                    self.inventory.SetEquipmentIndex(ownerEquipIndex, false);
-                }
+                self.inventory.SetEquipmentIndex(ownerEquipment, isRemovingEquipment: true);
             }
-
-            LastOwnerInventoryState = ownerState;
         }
 
-        private List<ItemInfo> BuildInventoryState(Inventory source)
+        public void ChannelOwnerInventory(CharacterMaster self, Inventory ownerInventory)
         {
-            List<ItemInfo> list = new();
+            int combinedItemsCount = 0;
+            using var _1 = ItemCatalog.PerItemBufferPool.RequestTemp<ItemIndex>(out var combinedItemsAcquired);
+            using var _2 = ItemCatalog.PerItemBufferPool.RequestTemp<bool>(out var indicesToUpdateSet);
+            using var _3 = new Inventory.InventoryChangeScope(self.inventory);
 
-            foreach (ItemIndex index in source.itemAcquisitionOrder)
+            var selfIndicies = self.inventory.channeledItemStacks.GetNonZeroIndicesSpan();
+            for (int i = 0; i < selfIndicies.Length; i++)
             {
-                list.Add(new()
-                {
-                    index = index,  
-                    count = source.GetItemCountPermanent(index)
-                });
+                AddItem(selfIndicies[i]);
             }
 
-            return list;
+            var ownerIndicies = self.inventory.permanentItemStacks.GetNonZeroIndicesSpan();
+            for (int i = 0; i < ownerIndicies.Length; i++)
+            {
+                AddItem(ownerIndicies[i]);
+            }
+
+            var itemsAcquiredSpan = combinedItemsAcquired.AsSpan(0, combinedItemsCount);
+            for (int i = 0; i < itemsAcquiredSpan.Length; i++)
+            {
+                var itemIndex = itemsAcquiredSpan[i];
+
+                // this is your running count of given items
+                // but i cant add an instance field, so im not bothering with covering this edgecase
+                // 
+                // private int[] previouslyProvidedItemBuffer = ItemCatalog.PerItemBufferPool.Request<int>();
+                // ...
+                int providedCount = self.inventory.GetItemCountChanneled(itemIndex);
+                // ref int providedCount = ref previouslyProvidedItemBuffer[(int)itemIndex];
+
+                int targetProvidedCount = ItemFilter(itemIndex) ? ownerInventory.GetItemCountPermanent(itemIndex) : 0;
+                int itemCountToProvide = targetProvidedCount - providedCount;
+
+                // for whatever reason, GiveItemChanneledImpl has an immutable property to allow negative item stacks.
+                // fuck that. clamp above zero.
+                int actualItemCount = self.inventory.GetItemCountChanneled(itemIndex);
+                itemCountToProvide = Math.Max(0, actualItemCount + itemCountToProvide) - actualItemCount;
+
+                if (itemCountToProvide != 0)
+                {
+                    self.inventory.ChangeItemStacksCount(new Inventory.GiveItemChanneledImpl
+                    {
+                        inventory = self.inventory
+                    }, itemIndex, itemCountToProvide);
+                    providedCount = targetProvidedCount;
+                }
+            }
+
+            void AddItem(ItemIndex itemIndex)
+            {
+                if (!indicesToUpdateSet[(int)itemIndex])
+                {
+                    indicesToUpdateSet[(int)itemIndex] = true;
+                    combinedItemsAcquired[combinedItemsCount++] = itemIndex;
+                }
+            }
         }
 
         private bool ItemFilter(ItemIndex index)
