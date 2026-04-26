caf22f18bec2356e19b285c71d58d93c0840461b
Snowy Snowtime <snowysnowtime1@gmail.com>
2026-04-25 20:27:28 -0700

    fix and update base config

--- SnowtimeToybox_Script/Components/PassiveTurretlingUpdateNamePerCharacter.cs
+++ SnowtimeToybox_Script/Components/PassiveTurretlingUpdateNamePerCharacter.cs
@@ -7,32 +7,33 @@ using UnityEngine.Networking;
 using Object = UnityEngine.Object;
 
 namespace SnowtimeToybox.Components;
-[RequireComponent(typeof(CharacterMaster))]
+[RequireComponent(typeof(CharacterBody))]
 public class PassiveTurretlingUpdateNamePerCharacter : MonoBehaviour
 {
-    public CharacterMaster self;
+    public CharacterBody self;
     public CharacterBody ownerBody;
-    public CharacterBody charBody;
+    public CharacterMaster charMaster;
     public bool nameUpdated;
 
     public void Awake()
     {
-        self = GetComponent<CharacterMaster>();
+        self = gameObject.GetComponent<CharacterBody>();
         nameUpdated = false;
     }
 
     public void FixedUpdate()
     {
         if (nameUpdated) return;
-        if (!self.GetBody()) return;
-        charBody = self.GetBody();
-        if (!charBody.master.minionOwnership.ownerMaster) return;
-        if (!charBody.master.minionOwnership.ownerMaster.GetBody()) return;
+        if (!self.master) return;
+        charMaster = self.master;
+        if (!charMaster.minionOwnership.ownerMaster) return;
+        if (!charMaster.minionOwnership.ownerMaster.GetBody()) return;
         //Log.Debug(charBody.master.minionOwnership.ownerMaster.GetBody().name);
-        ownerBody = charBody.master.minionOwnership.ownerMaster.GetBody();
+        ownerBody = charMaster.minionOwnership.ownerMaster.GetBody();
         
         Log.Debug(ownerBody.name + " turretling ");
-        charBody.baseNameToken = $"TURRETLING_{ownerBody.name.ToUpper()}_NAME";
+        string ownerName = ownerBody.name.Replace("(Clone)", "");
+        self.baseNameToken = $"TURRETLING_{ownerName.ToUpper()}_NAME";
         nameUpdated = true;
     }
 }
